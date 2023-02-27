554cf56fe9774788f7fffdcd7f31c8288d2ae76b
marcossnikel <marcosnikeldev@gmail.com>
2023-02-27 06:51:31 -0300

    SOLUTION REMOVED

--- Python Sintax Guide/2dArrays.py
+++
@@ -1,4 +0,0 @@
-#2-D lISTS
-
-arr = [[0] * 4 for i in range(4)]
-print(arr)
--- Python Sintax Guide/list comprehension.py
+++
@@ -1,7 +0,0 @@
-#initialize lists
-
-arr = [i for i in range(5)]
-print(arr)
-
-arr = [i+i for i in range(5)]
-print(arr)
